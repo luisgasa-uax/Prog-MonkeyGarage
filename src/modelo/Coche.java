17f4feebde98f2917f13a1a7b4ca3e0a2cd39752
Luis García <luisgasa.uax@gmail.com>
2026-04-23 09:20:43 +0200

    feat Coche: atributos final y setters

--- src/modelo/Coche.java
+++ src/modelo/Coche.java
@@ -1,4 +1,70 @@
 package modelo;
 
 public class Coche {
+    private final int id;
+    private String matricula;
+    private final String marca;
+    private final String modelo;
+    private int potencia; //CV
+    private int km;
+    private double precio;
+    private int cantidadReparaciones;
+
+
+    public Coche(int id, String matricula, String marca, String modelo, int potencia, int km, double precio) {
+        this.id = id;
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.potencia = potencia;
+        this.km = km;
+        this.precio = precio;
+        this.cantidadReparaciones = 0;
+    }
+
+    // los getters son (generalmente) públicos, devuelven valores, y no reciben nada
+    public int getId(){
+        return this.id;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public int getKm() {
+        return km;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getCantidadReparaciones() {
+        return cantidadReparaciones;
+    }
+
+    // Setters no creamos setters para todos los atributos, sólamente para los que pueden cambiar
+    // devuelven: nada
+    // reciben: el nuevo valor correspondiente
+
+    /**
+     * Establece el nuevo precio del coche
+     * No devuelve nada
+     * @param precio nuevo precio a establecer
+     */
+    public void setPrecio(double precio){
+        this.precio = precio;
+    }
 }
